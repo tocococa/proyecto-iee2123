1b445282eaa3057389f67d58d08bf361ae4e77cd
tocococa <tocococa@gmail.com>
2020-11-04 00:00:31 -0300

    read-write to buts done

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+
+
 def to_bits(path_in, path_out):
+    path_in = os.path.join(os.getcwd(), path_in)
+    path_out = os.path.join(os.getcwd(), path_out)
     with open(path_in, 'rb') as file:
         line = file.readline()
         files_out = {x: [] for x in range(8)}
@@ -6,3 +11,7 @@ def to_bits(path_in, path_out):
             chunk = bytes_file[i:i+8]
             for k in range(len(chunk)):
                 files_out[k].append(chunk[k])
+    for k in files_out.keys():
+        path_file = path_out + f'{k}b.txt'
+        with open(path_file + f'{k}b.txt', 'wb') as file:
+            file.write(*files_out[k])
